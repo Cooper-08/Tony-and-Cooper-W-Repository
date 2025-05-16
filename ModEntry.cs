2921d96e99f3f2a3eaefbb677063727e83ae401a
Tony Gao <tony_gao@horacemann.org>
2025-05-16 12:41:56 -0400

    tp methods finished

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Transactions;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -13,6 +14,7 @@ namespace friendTP
     internal sealed class ModEntry : Mod
     {
         IModHelper? hp = null;
+        
         public override void Entry(IModHelper helper) {
             hp = helper;
             helper.Events.GameLoop.TimeChanged += this.OnTimeChanged;
@@ -23,20 +25,26 @@ namespace friendTP
             int currentTime = e.NewTime;
             if(currentTime % 600 == 0) {
                 Monitor.Log("6 hours have passed", LogLevel.Info);
-                foreach(var farmer in Game1.getOnlineFarmers()) {
-                if(farmer != null) {
-                    Monitor.Log(farmer.Name, LogLevel.Info);
-                }
-            }
+                tpPlayer(sender);
             }
         }
 
         public void tpPlayer(object? sender) {
-            
+            Random rand = new Random();
+            List<StardewValley.Farmer> onlineFarmers = getOnlineFarmers(sender);
+            onlineFarmers.Remove(Game1.player);
+            StardewValley.Farmer target = onlineFarmers[rand.Next(onlineFarmers.Count)];
+            Vector2 targetTile = getTileLocation(target);
+            Game1.warpFarmer(target.currentLocation.Name, (int) targetTile.X, (int) targetTile.Y, false);
+            Game1.chatBox.addMessage($"Teleported {Game1.player.Name} to {target.Name}", Color.White);
         }
 
         public List<StardewValley.Farmer> getOnlineFarmers(object? sender) {
             return Game1.getOnlineFarmers().ToList();
+        }
+
+        private static Vector2 getTileLocation(StardewValley.Farmer farmer) {
+            return farmer.Position / 64f;
         }
     }
 }
